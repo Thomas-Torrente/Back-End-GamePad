022887caedd605870b04232139fa51e2f5e76066
Thomas Torrente <thomastorrente@imac-de-thomas.home>
2020-12-07 16:29:05 +0100

    write comments

---
+++ models/Review.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Review = mongoose.model("Review", {
+  Review_title: String,
+  Review_comments: String,
+  Review_sreen: { type: mongoose.Schema.Mixed, default: {} },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = Review;
