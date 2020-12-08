7e508255f2c149a632ae8553bb1ef0f3a45d1488
Thomas Torrente <thomastorrente@imac-de-thomas.home>
2020-12-08 15:10:05 +0100

    resolve crash back

--- routes/review.js
+++ routes/review.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const cloudinary = require("cloudinary").v2;
+
+// const isAuthenticated = require("../middlewares/isAuthenticated");
 
 const Review = require("../models/Review");
-const User = require("../models/User");
-// router.post("/review/:slug", async (req, res) => {
-//   try {
-//     const { title, comments, screen } = req.fields;
+//isAuthenticated
+router.post("/:slug/post", async (req, res) => {
+  try {
+    const newComment = new Review({
+      Review_title: title,
+      Review_comments: comments,
+      owner: req.user,
+    });
 
-//     const newComment = new Review({
-//       Review_title: title,
-//       Review_comments: comments,
-//       owner: req.user,
-//     });
+    await newComment.save();
 
-//     await newComment.save();
-//     res.json(newComment);
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-// });
+    res.json(newComment);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
---
+++ test-review.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const cloudinary = require("cloudinary").v2;
+
+// const isAuthenticated = require("../middlewares/isAuthenticated");
+
+const Review = require("../models/Review");
+//isAuthenticated
+router.post("/review/:slug", async (req, res) => {
+  // permet de créer un commentaire si user is authentifier
+  try {
+    const { title, comments, screen } = req.fields;
+    //  add screen quand le reste est ok
+
+    const newComment = new Review({
+      Review_title: title,
+      Review_comments: comments,
+      owner: req.user,
+    });
+    // on veut que dans la création du commentaire yest un titre, le commentaire, et des screenshot si il veut
+
+    // const result = await cloudinary.uploader.upload(req.files.picture.path, {
+    //   // stock le sceen envoyer dans un fichier cloudinary gamepad
+    // });
+    // newComment.Review_screen = result;
+    // on dit que le screen du commentaire est = a result
+    await newComment.save();
+    //   on enregistre le review
+    res.json(newComment);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// router.get("/reviews/:slug", async (req, res) => {
+//   try {
+// const reviewSlug = await Review.find
+
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// });
